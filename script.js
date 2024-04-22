09d4bdc31bb45d9e476eb4cdc4707727fd674c8d
Eunji Park(EJ) <76797880+guide333@users.noreply.github.com>
2024-04-22 21:53:23 +0900

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ function init() {
 };
 
 async function getCode(questCode) {
-  const Url = `generate`;//클라우드 타입 서버 주소 입력
+  const Url = `https://port-0-idea-chatbot-1pgyr2mlvaydykk.sel5.cloudtype.app/`;//클라우드 타입 서버 주소 입력
   const Data = JSON.stringify({ userInput: questCode });   
   let response; 
     try {
